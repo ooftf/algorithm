21fcbeb7ec81e0d1016c8318b76e41e59decd27d
ooftf <994749769@qq.com>
2021-05-05 21:54:36 +0800

    JsonTest

--- app/src/main/java/com/ooftf/algorithm/leetcode/IsMatch.kt
+++ app/src/main/java/com/ooftf/algorithm/leetcode/IsMatch.kt
@@ -3,13 +3,41 @@ package com.ooftf.algorithm.leetcode
 /**
  * https://leetcode-cn.com/problems/regular-expression-matching/
  *
+ * 动态规划
+ *
  * 太难了，等等再做
  */
 object IsMatch {
 
-
+    // s = "aaa" p = "a*aaa"
     fun isMatch(s: String, p: String): Boolean {
-        TODO()
+        var i = 0
+        var j = 0
+        while ((i < s.length || j < p.length) && j < p.length) {
+            if (j + 1 < p.length && p[j + 1] == '*') {
+                if (match(s, p, i, j)) {
+                    i++
+                } else {
+                    j += 2
+                }
+            } else {
+                if (match(s, p, i, j)) {
+                    j++
+                    i++
+                } else {
+                    return false
+                }
+            }
+        }
+        return i == s.length
+        //return true
+    }
+
+    fun match(s: String, p: String, i: Int, j: Int): Boolean {
+        if (i >= s.length) {
+            return false
+        }
+        return s[i] == p[j] || p[j] == '.'
     }
 
     @JvmStatic
