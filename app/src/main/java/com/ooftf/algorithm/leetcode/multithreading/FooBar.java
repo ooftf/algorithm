2c23045c1d01f86943c164eeef7c3674533eeb56
ooftf <994749769@qq.com>
2021-07-14 01:12:35 +0800

    多线程

--- app/src/main/java/com/ooftf/algorithm/leetcode/multithreading/FooBar.java
+++ app/src/main/java/com/ooftf/algorithm/leetcode/multithreading/FooBar.java
@@ -1,5 +1,15 @@
 package com.ooftf.algorithm.leetcode.multithreading;
 
+/**
+ * 两个不同的线程将会共用一个 FooBar 实例。其中一个线程将会调用 foo() 方法，另一个线程将会调用 bar() 方法。
+ *
+ * 请设计修改程序，以确保 "foobar" 被输出 n 次。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/print-foobar-alternately
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
 class FooBar {
     private int n;
     boolean foo = true;
